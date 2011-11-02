5c08cd5e72772169adc8e23cc69f9aa27a9c9633
Dag Erik Vikan <dvikan@gmail.com>
2011-11-02 12:50:56 +0100

    yeah

--- src/CPU.java
+++ src/CPU.java
@@ -4,5 +4,54 @@
  */
 public class CPU implements Constants {
 	
+	private Queue queue;
+	
+	private Process cp;
+
+	private Gui gui;
+	
+	public CPU(Queue cpuQueue, Gui gui) {
+		
+		this.queue = cpuQueue;
+		this.gui = gui;
+	}
+
+	public Process startNextProcess() {
+		
+		cp = getNextProcess();
+		gui.setCpuActive(cp); // gui
+	
+		return cp;		
+	}
+	
+	public Process stopCurrentProcess() {
+		
+		gui.setCpuActive(null); // gui
+		
+		Process tmp = cp;
+		cp = null;
+		return tmp;
+	}
+	
+	public void insertProcess(Process p) {
+		queue.insert(p);
+	}
+	
+	public Process getNextProcess() {
+		
+		return (Process) queue.removeNext();
+	}
+	
+	public void timePassed(long timePassed) {
+		
+		// register cpu idle time
+		if(cp == null) {
+			
+			Statistics.cpuIdleTime(timePassed);
+		}
+			
+		Statistics.cpuQueueLengthTime(queue.getQueueLength(), timePassed);
+		Statistics.cpuQueueLength(queue.getQueueLength());
+	}
 	
 }
